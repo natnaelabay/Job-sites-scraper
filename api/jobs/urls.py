708a0c1175afca882cd7ac6ccba22433ef27e79d
Natnael Abay <se.natnael.abay@gmail.com>
2023-10-01 02:59:20 +0300

    feat: stat based on nlp

--- api/jobs/urls.py
+++ api/jobs/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from jobs.views import JobListingView
+from jobs.views import JobListingView, stats
 
 
 urlpatterns = [
-    path("jobs/", JobListingView.as_view(), name="job_listing")
+    path("jobs/", JobListingView.as_view(), name="job_listing"),
+    path("jobs/stats/", stats, name="job_listing"),
 ]
--- api/jobs/views.py
+++ api/jobs/views.py
@@ -8,6 +8,8 @@ from rest_framework.generics import ListAPIView
 from jobs.serializers import JobListingSerializer
 from jobs.models import JobListing
 from jobs.filters import JobListFilter
+import spacy
+
 
 
 class JobListingView(ListAPIView):
@@ -15,3 +17,51 @@ class JobListingView(ListAPIView):
     serializer_class = JobListingSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_class = JobListFilter
+
+
+
+@api_view(["GET"])
+def stats(request):
+    jobs = JobListing.objects.all()
+    roles = [
+        ("Frontend","Frontend Engineer", "Frontend Developer"), 
+        ("Backend","Backend Engineer", "Backend Developer"), 
+        ("Product Manager","Product Developer", "Product Designer"), 
+        ("Project Manager","Team Lead", "Senior Developer"), 
+        ("Full Stack","Full-Stack", "Full stack Engineer", "Full Stack Developer"), 
+        ("Designer","UI", "UI/UX", "Frontend Engineer"), 
+    ]
+    stat = {
+        0 : 0,
+        1 : 0,
+        2 : 0,
+        3 : 0,
+        4 : 0,
+        5 : 0,
+    }
+    roles_stat = {
+        0 : [],
+        1 : [],
+        2 : [],
+        3 : [],
+        4 : [],
+        5 : [],
+    }
+
+    nlp = spacy.load("en_core_web_sm")
+    for job in jobs.iterator():
+        _w = nlp(job.title)
+        cur = []
+
+        for role in roles:
+            _temp_score = []
+            for detail in role:
+                _temp_score.append(_w.similarity(nlp(detail)))
+            
+            cur.append(max(_temp_score))
+
+        _max = max(cur)
+        idx = cur.index(_max)
+        stat[idx] += 1
+
+    return Response(stat, status=status.HTTP_200_OK)
