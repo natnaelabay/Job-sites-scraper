02b9c52f28396ea2dd7f32335586908cbe537d40
Natnael Abay <se.natnael.abay@gmail.com>
2023-10-01 03:39:20 +0300

    patch: filter patch

--- api/jobs/filters.py
+++ api/jobs/filters.py
@@ -5,12 +5,13 @@ from django.db.models import Q
 
 class JobListFilter(django_filters.FilterSet):
     tags_contains = django_filters.CharFilter(method='filter_tags_contains')
+    title = django_filters.CharFilter(lookup_expr='icontains')
+    company = django_filters.CharFilter(lookup_expr='icontains')
 
     class Meta:
         model = JobListing
         fields = {
-            "title": ["exact", "icontains"],
-            "company": ["exact", "icontains"],
+            # "company": ["exact", "icontains"],
             "date": ["exact", "lt", "gt"],
             "source": ["exact", "icontains"],
             "location": ["exact", "icontains"],
