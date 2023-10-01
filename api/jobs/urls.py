3e87e11779f5b5f4cb6384c097aa56de94bb8b41
Natnael Abay <se.natnael.abay@gmail.com>
2023-10-01 03:26:18 +0300

    patch: limit the work load on stat calculators

--- api/jobs/urls.py
+++ api/jobs/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from jobs.views import JobListingView, stats
+from jobs.views import JobListingView, role_stats, stack_stats 
 
 
 urlpatterns = [
     path("jobs/", JobListingView.as_view(), name="job_listing"),
-    path("jobs/stats/", stats, name="job_listing"),
+    path("jobs/role-stats/", role_stats, name="role_stats"),
+    path("jobs/stack-stats/", stack_stats, name="stack_stats"),
 ]
--- api/jobs/views.py
+++ api/jobs/views.py
@@ -11,7 +11,6 @@ from jobs.filters import JobListFilter
 import spacy
 
 
-
 class JobListingView(ListAPIView):
     queryset = JobListing.objects.all()
     serializer_class = JobListingSerializer
@@ -19,25 +18,24 @@ class JobListingView(ListAPIView):
     filterset_class = JobListFilter
 
 
-
 @api_view(["GET"])
-def stats(request):
-    jobs = JobListing.objects.all()
+def role_stats(request):
+    jobs = JobListing.objects.all()[:30]
     roles = [
-        ("Frontend","Frontend Engineer", "Frontend Developer"), 
-        ("Backend","Backend Engineer", "Backend Developer"), 
-        ("Product Manager","Product Developer", "Product Designer"), 
-        ("Project Manager","Team Lead", "Senior Developer"), 
-        ("Full Stack","Full-Stack", "Full stack Engineer", "Full Stack Developer"), 
-        ("Designer","UI", "UI/UX", "Frontend Engineer"), 
+        ("Frontend", "Frontend Engineer", "Frontend Developer"),
+        ("Backend", "Backend Engineer", "Backend Developer"),
+        ("Product Manager", "Product Developer", "Product Designer"),
+        ("Project Manager", "Team Lead", "Senior Developer"),
+        ("Full Stack", "Full-Stack", "Full stack Engineer", "Full Stack Developer"),
+        ("Designer", "UI", "UI/UX", "Frontend Engineer"),
     ]
     stat = {
-        0 : 0,
-        1 : 0,
-        2 : 0,
-        3 : 0,
-        4 : 0,
-        5 : 0,
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
     }
 
     nlp = spacy.load("en_core_web_sm")
@@ -49,7 +47,7 @@ def stats(request):
             _temp_score = []
             for detail in role:
                 _temp_score.append(_w.similarity(nlp(detail)))
-            
+
             cur.append(max(_temp_score))
 
         _max = max(cur)
@@ -57,15 +55,81 @@ def stats(request):
         stat[idx] += 1
 
     _stat = {
-        0 : "Frontend",
-        1 : "Backend",
-        2 : "Product Manager",
-        3 : "Project Manager",
-        4 : "Full Stack",
-        5 : "Designer",
+        0: "Frontend",
+        1: "Backend",
+        2: "Product Manager",
+        3: "Project Manager",
+        4: "Full Stack",
+        5: "Designer",
     }
+
     mapped = {}
     for key in stat.keys():
         mapped[_stat[key]] = stat[key]
 
+    return Response(mapped, status=status.HTTP_200_OK)
+
+
+
+@api_view(["GET"])
+def stack_stats(request):
+    jobs = JobListing.objects.all()[:30]
+    roles = [
+        ("React","React.js", "React Developer"), 
+        ("Angular","Angular.js", "Angular Developer"), 
+        ("Python","Python Developer", "Machine learning and AI", "Backend Engineer"), 
+        ("Typescript","TypeScript Developer"), 
+        ("Crypto","Web3", "blockchain", "Blockchain Developer"), 
+        ("Node Js","express", "API", "Backend Engineer"), 
+        ("Vue","Vue.js", "Vue Developer"), 
+    ]
+    stat = {
+        0 : 0,
+        1 : 0,
+        2 : 0,
+        3 : 0,
+        4 : 0,
+        5 : 0,
+        6 : 0,
+    }
+    grouped_ds = {
+        0 : [],
+        1 : [],
+        2 : [],
+        3 : [],
+        4 : [],
+        5 : [],
+        6 : [],
+    }
+
+    nlp = spacy.load("en_core_web_sm")
+    for job in jobs:
+        _w = nlp(job.title)
+        cur = []
+
+        for role in roles:
+            a = []
+            for detail in role:
+                a.append(_w.similarity(nlp(detail)))
+            cur.append(max(a))
+
+        _max = max(cur)
+        idx = cur.index(_max)
+        stat[idx] += 1
+        # grouped_ds[idx].append(job)
+
+    _stat = {
+        0 : "React",
+        1 : "Angular",
+        2 : "Python",
+        3 : "Typescript",
+        4 : "Web 3",
+        5 : "Node JS",
+        6 : "Vue",
+    }
+    mapped = {}
+    for key in stat.keys():
+        mapped[_stat[key]] = stat[key]
+        grouped_ds[_stat[key]] = grouped_ds[key]
+        
     return Response(mapped, status=status.HTTP_200_OK)
